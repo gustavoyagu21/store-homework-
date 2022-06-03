b0aafcd11e36c66f6131b95cd3bc109a68bb1625
gustavoyagu21 <75746564+gustavoyagu21@users.noreply.github.com>
2022-06-03 01:17:59 -0500

    Add files via upload

---
+++ tienda2021_1.sql
@@ -0,0 +1,109 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.24-MariaDB : Database - tienda2021_1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tienda2021_1` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `tienda2021_1`;
+
+/*Table structure for table `clientes` */
+
+DROP TABLE IF EXISTS `clientes`;
+
+CREATE TABLE `clientes` (
+  `idc` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `fnacimiento` date DEFAULT NULL,
+  `detalle` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`idc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `clientes` */
+
+/*Table structure for table `detalles` */
+
+DROP TABLE IF EXISTS `detalles`;
+
+CREATE TABLE `detalles` (
+  `idd` int(11) NOT NULL AUTO_INCREMENT,
+  `cantidad` int(11) DEFAULT NULL,
+  `precioventa` decimal(10,0) DEFAULT NULL,
+  `importe` decimal(10,0) DEFAULT NULL,
+  `idproducto` int(11) DEFAULT NULL,
+  `idfactura` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idd`),
+  KEY `idproducto` (`idproducto`),
+  KEY `idfactura` (`idfactura`),
+  CONSTRAINT `detalles_ibfk_1` FOREIGN KEY (`idproducto`) REFERENCES `productos` (`idp`),
+  CONSTRAINT `detalles_ibfk_2` FOREIGN KEY (`idfactura`) REFERENCES `facturas` (`idf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `detalles` */
+
+/*Table structure for table `facturas` */
+
+DROP TABLE IF EXISTS `facturas`;
+
+CREATE TABLE `facturas` (
+  `idf` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `subtotal` decimal(10,0) DEFAULT NULL,
+  `IVA` decimal(10,0) DEFAULT NULL,
+  `total` decimal(10,0) DEFAULT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idf`),
+  KEY `idcliente` (`idcliente`),
+  CONSTRAINT `facturas_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `clientes` (`idc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `facturas` */
+
+/*Table structure for table `productos` */
+
+DROP TABLE IF EXISTS `productos`;
+
+CREATE TABLE `productos` (
+  `idp` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `marca` varchar(50) DEFAULT NULL,
+  `detalle` varchar(50) DEFAULT NULL,
+  `precio` decimal(10,0) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `foto` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`idp`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `productos` */
+
+insert  into `productos`(`idp`,`nombre`,`marca`,`detalle`,`precio`,`stock`,`foto`) values (1,' candado','forte','buen candado ',5,5,'Candado-50-alta-seguridada.jpg'),(2,' martillo','mart','martillo premium',20,7,'40200029PNM001G.png');
+
+/*Table structure for table `usuarios` */
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `idu` int(11) NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `usuario` varchar(50) DEFAULT NULL,
+  `clave` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `usuarios` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
